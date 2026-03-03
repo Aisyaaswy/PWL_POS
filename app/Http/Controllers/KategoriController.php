65e10e7878517af9734262a09af27de23684310b
Aisyaaswy <aisyaaidha@gmail.com>
2026-03-03 22:10:03 +0700

    Eloquent ORM

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,13 +9,13 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        /* $data = [
-            'kategori_kode' => 'HBR',
-            'kategori_nama' => 'Hiburan',
-            'created_at' => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil'; */
+        //  $data = [
+        //     'kategori_kode' => 'HBR',
+        //     'kategori_nama' => 'Hiburan',
+        //     'created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil'; 
 
         // $row = DB::table('m_kategori')
         //     ->where('kategori_kode', 'HBR')
